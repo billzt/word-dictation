e1572c333878179ed7bb7af61b4043f189fb801f
billzt <ztstillwater@protonmail.com>
2021-02-04 23:25:02 +0800

    Update dictation.py

--- dictation.py
+++ dictation.py
@@ -9,9 +9,16 @@ import os
 
 current_num = 1
 previous_line = ''
+now_dictation_items = []
+def save_answer():
+    with open('dictation_answer.txt', mode='w') as f_answer:
+        for i in range(len(now_dictation_items)):
+            print(f'({i+1}) {now_dictation_items[i]}', file=f_answer)
+        f_answer.close()
+        print(f'Answers saved to dictation_answer.txt', file=sys.stderr)
 
 class SayItem(cmd.Cmd):
-    intro = 'Welcome to the dictation shell.  Type n to listen the next item, or r to repeat the previous item.\n'
+    intro = 'Welcome to the dictation shell.  Type n to listen the next item, or r to repeat the previous item, or s to save current answers.\n'
     prompt = '(dictation) '
     def do_n(self, line):
         """n (means next)
@@ -23,10 +30,12 @@ class SayItem(cmd.Cmd):
             print(f"Say {current_num}/{all_num} by voice: {voices[current_num%len(voices)]}")
             os.system(f'say -v {voices[current_num%len(voices)]} {items[0]}')
             previous_line = items[0]
+            now_dictation_items.append(items[0])
             del(items[0])
             current_num += 1
         else:
             print('All finished')
+            save_answer()
             return True
 
     def do_r(self, line):
@@ -38,7 +47,13 @@ class SayItem(cmd.Cmd):
             print(f"Say {current_num-1}/{all_num} in {voices[(current_num-1)%len(voices)]}")
             os.system(f'say -v {voices[(current_num-1)%len(voices)]} {previous_line}')
 
+    def do_s(self, line):
+        """s (means save)
+        s (means save)"""
+        save_answer()
+
     def do_EOF(self, line):
+        save_answer()
         return True
 
     def postloop(self):
